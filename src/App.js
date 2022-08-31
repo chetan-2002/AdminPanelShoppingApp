f7c32f3d4989526809fc1d83adfaef17027bb230
chetan-2002 <77783033+chetan-2002@users.noreply.github.com>
2022-08-31 17:35:24 +0530

    some base files addde

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect } from "react";
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import Login from "./components/Login";
+import Home from "./components/Home";
+import AddProduct from "./components/Admin/AddProduct";
+import AddCategory from "./components/Admin/AddCategory";
+import DeleteCategory from "./components/Admin/DeleteCategory";
+import DeleteProduct from "./components/Admin/DeleteProduct";
+import UpdateProduct from "./components/Admin/UpdateProduct";
+import Logout from "./components/Logout";
 
 function App() {
+  useEffect(() => {
+    localStorage.setItem("chakra-ui-color-mode", "dark");
+  }, []);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        <Route path="/" element={<Login />} exact />
+        <Route path="/home" element={<Home />} exact />
+        <Route path="/addaproduct" element={<AddProduct />} exact />
+        <Route path="/addacategory" element={<AddCategory />} exact />
+        <Route path="/deleteacategory" element={<DeleteCategory />} exact />
+        <Route path="/deleteaproduct" element={<DeleteProduct />} exact />
+        <Route path="/updateaproduct" element={<UpdateProduct />} exact />
+        <Route path="/logout" element={<Logout />} exact />
+        <Route path="*" element={<Login />} />
+      </Routes>
     </div>
   );
 }
