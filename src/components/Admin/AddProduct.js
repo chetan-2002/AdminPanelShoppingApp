def02c1767e3d686f9d1b362cb1f468b5718f0f8
chetan-2002 <77783033+chetan-2002@users.noreply.github.com>
2022-08-31 17:39:44 +0530

    component for adding a product added

---
+++ src/components/Admin/AddProduct.js
@@ -0,0 +1,289 @@
+import {
+  Box,
+  Button,
+  Flex,
+  FormControl,
+  FormLabel,
+  Input,
+  Stack,
+  Text,
+  useColorModeValue,
+  useToast,
+} from "@chakra-ui/react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import AllCategoriesDropdown from "./AllCategoriesDropdown";
+
+const AddProduct = () => {
+  const navigate = useNavigate();
+  const [categoriesList, setCategoriesList] = useState([]);
+  const toast = useToast();
+  const fetchCategories = async () => {
+    const temp = [];
+    const config = {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${
+          JSON.parse(localStorage.getItem("userInfo")).token
+        }`,
+      },
+    };
+    axios
+      .get("http://localhost:5000/api/category", config)
+      .then((res) => {
+        for (let i = 0; i < res.data.length; i++) {
+          temp.push(res.data[i].name);
+        }
+        setCategoriesList(temp);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+  useEffect(() => {
+    const user = JSON.parse(localStorage.getItem("userInfo"));
+    fetchCategories();
+    if (!user) {
+      navigate("/");
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+  //eslint-disable-next-line
+  const [pic, setPic] = useState();
+  const [picLoading, setPicLoading] = useState(false);
+  const [product, setProduct] = useState({
+    name: "",
+    price: "",
+    description: "",
+    image: "",
+    categoryName: "",
+  });
+  const postDetails = (pics) => {
+    setPicLoading(true);
+    if (pics === undefined) {
+      toast({
+        title: "Please Select an Image!",
+        status: "warning",
+        duration: 5000,
+        isClosable: true,
+        position: "bottom",
+      });
+      setPicLoading(false);
+      return;
+    }
+    if (pics.type === "image/jpeg" || pics.type === "image/png") {
+      const data = new FormData();
+      data.append("file", pics);
+      data.append("upload_preset", "chat_app");
+      data.append("cloud_name", "dvbjetffn");
+      fetch("https://api.cloudinary.com/v1_1/dvbjetffn/image/upload", {
+        method: "post",
+        body: data,
+      })
+        .then((res) => res.json())
+        .then((data) => {
+          setPic(data.url.toString());
+          setPicLoading(false);
+          toast({
+            title: "Image Uploaded Successfully!",
+            status: "success",
+            duration: 5000,
+            isClosable: true,
+            position: "bottom",
+          });
+          setProduct({ ...product, image: data.url.toString() });
+        })
+        .catch((err) => {
+          toast({
+            title: "Error Uploading Image!",
+            status: "error",
+            duration: 5000,
+            isClosable: true,
+            position: "bottom",
+          });
+          setPicLoading(false);
+        });
+    } else {
+      toast({
+        title: "Please Select an Image!",
+        status: "warning",
+        duration: 5000,
+        isClosable: true,
+        position: "bottom",
+      });
+      setPicLoading(false);
+      return;
+    }
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setPicLoading(true);
+    if (
+      !product.name ||
+      !product.price ||
+      !product.description ||
+      !product.image ||
+      !product.categoryName
+    ) {
+      toast({
+        title: "Please Fill All Fields!",
+        status: "warning",
+        duration: 5000,
+        isClosable: true,
+        position: "bottom",
+      });
+      setPicLoading(false);
+      return;
+    }
+    if (product.categoryName === "Select a Category") {
+      toast({
+        title: "Please Select a valid Category!",
+        status: "warning",
+        duration: 5000,
+        isClosable: true,
+        position: "bottom",
+      });
+      setPicLoading(false);
+      return;
+    }
+    const config = {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${
+          JSON.parse(localStorage.getItem("userInfo")).token
+        }`,
+      },
+    };
+    axios
+      .post("http://localhost:5000/api/product/addProduct", product, config)
+      .then((res) => {
+        toast({
+          title: "Product Added Successfully!",
+          status: "success",
+          duration: 5000,
+          isClosable: true,
+          position: "bottom",
+        });
+        setPicLoading(false);
+        setProduct({
+          name: "",
+          price: "",
+          description: "",
+          image: "",
+          categoryName: "",
+        });
+      })
+      .catch((err) => {
+        toast({
+          title: "Error Adding Product!",
+          status: "error",
+          duration: 5000,
+          isClosable: true,
+          position: "bottom",
+        });
+        setPicLoading(false);
+      });
+  };
+  return (
+    <Flex
+      minH={"100vh"}
+      align={"center"}
+      justify={"center"}
+      bg={useColorModeValue("gray.50", "gray.800")}
+    >
+      <Stack spacing={8} mx={"auto"} maxW={"lg"} py={12} px={6}>
+        <Stack align={"center"}>
+          <Box fontSize={"4xl"} display={"flex"}>
+            <Text
+              textAlign={"center"}
+              fontSize={"4xl"}
+              fontWeight={"extrabold"}
+            >
+              Add a new product to the Database
+            </Text>
+          </Box>
+        </Stack>
+        <Box
+          rounded={"lg"}
+          bg={useColorModeValue("white", "gray.700")}
+          boxShadow={"lg"}
+          p={8}
+        >
+          <Stack spacing={4}>
+            <FormControl id="Name" isRequired>
+              <FormLabel>Name</FormLabel>
+              <Input
+                type="text"
+                placeholder="Enter Product Name"
+                value={product.name}
+                onChange={(e) =>
+                  setProduct({ ...product, name: e.target.value })
+                }
+              />
+            </FormControl>
+            <FormControl isRequired>
+              <FormLabel>Description</FormLabel>
+              <Input
+                type="text"
+                placeholder="Enter Product Description"
+                value={product.description}
+                onChange={(e) =>
+                  setProduct({ ...product, description: e.target.value })
+                }
+              />
+            </FormControl>
+            <FormControl isRequired>
+              <FormLabel>Price</FormLabel>
+              <Input
+                type="number"
+                placeholder="Enter Product Price"
+                value={product.price}
+                onChange={(e) =>
+                  setProduct({ ...product, price: e.target.value })
+                }
+              />
+            </FormControl>
+            <FormControl id="pic" isRequired>
+              <FormLabel>Upload Product Picture</FormLabel>
+              <Input
+                type="file"
+                p={1.5}
+                accept="image/*"
+                onChange={(e) => {
+                  postDetails(e.target.files[0]);
+                }}
+              />
+            </FormControl>
+            <FormControl isRequired>
+              <FormLabel>Category</FormLabel>
+              <AllCategoriesDropdown
+                categoriesList={categoriesList}
+                value={product.category}
+                onChange={(e) =>
+                  setProduct({ ...product, categoryName: e.target.value })
+                }
+              />
+            </FormControl>
+            <Stack spacing={12}>
+              <Button
+                bg={"blue.400"}
+                color={"white"}
+                _hover={{
+                  bg: "blue.500",
+                }}
+                onClick={handleSubmit}
+                isLoading={picLoading}
+              >
+                Add the product to Database
+              </Button>
+            </Stack>
+          </Stack>
+        </Box>
+      </Stack>
+    </Flex>
+  );
+};
+
+export default AddProduct;
